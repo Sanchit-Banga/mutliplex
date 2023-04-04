ff3a738da12f1a4028bcb05cd770e437d6f5d9a4
imRajAryan09 <raj.12020162@lpu.in>
2023-04-04 17:34:54 +0530

    building microservice

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.bookingservice;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BookingServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,24 +4,26 @@ import com.example.userservice.filter.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
-	@Bean
-	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
-		FilterRegistrationBean<AuthFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-		filterRegistrationBean.setFilter(new AuthFilter());
-		filterRegistrationBean.addUrlPatterns("/api/v1/users/admin/*");
-		filterRegistrationBean.addUrlPatterns("/api/v1/users/booking/*");
-		return filterRegistrationBean;
-	}
+    @Bean
+    public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+        FilterRegistrationBean<AuthFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new AuthFilter());
+        filterRegistrationBean.addUrlPatterns("/api/v1/users/admin/*");
+        filterRegistrationBean.addUrlPatterns("/api/v1/users/booking/*");
+        return filterRegistrationBean;
+    }
 
 
 }
